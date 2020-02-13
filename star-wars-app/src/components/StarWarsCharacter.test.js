e9dc80e6b5954aff39e0ce7cda7095e7338aadd2
Jared Brown <jaredbrown1@hotmail.com>
2020-02-12 19:58:26 -0800

    added api test

--- star-wars-app/src/components/StarWarsCharacter.test.js
+++ star-wars-app/src/components/StarWarsCharacter.test.js
@@ -3,20 +3,42 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import StarWarsCharacters from "./StarWarsCharacters";
+import axios from "axios";
 
-test("that the next button loads", async () => {
-  const wrapper = rtl.render(<StarWarsCharacters />);
-  await wrapper.findByText(/Next/i);
-  const nextArr = wrapper.getByText(/Next/i);
-  rtl.act(() => {
-    rtl.fireEvent.click(nextArr);
-  });
-  expect(wrapper.getByTestId("next-button")).not.toBeNull();
+// test("that the next button loads", async () => {
+//   const wrapper = rtl.render(<StarWarsCharacters />);
+//   await wrapper.findByText(/Next/i);
+//   const nextArr = wrapper.getByText(/Next/i);
+//   rtl.act(() => {
+//     rtl.fireEvent.click(nextArr);
+//   });
+//   expect(wrapper.getByTestId("next-button")).not.toBeNull();
+// });
+
+// test("that the previous button loads", async () => {
+//   const wrapper = rtl.render(<StarWarsCharacters />);
+//   await wrapper.findByTestId("prev-button");
+//   const element = wrapper.getByText(/previous/i);
+//   expect(element).toBeVisible();
+// });
+
+jest.mock("axios", () => {
+  return {
+    get: jest.fn(() =>
+      Promise.resolve({
+        data: {
+          count: 0,
+          next: "",
+          previous: null,
+          results: [{}]
+        }
+      })
+    )
+  };
 });
 
-test("that the previous button loads", async () => {
+test("characters are loaded from api", async () => {
   const wrapper = rtl.render(<StarWarsCharacters />);
-  await wrapper.findByTestId("prev-button");
-  const element = wrapper.getByText(/previous/i);
-  expect(element).toBeVisible();
+  await wrapper.findByTestId("char");
+  expect(axios.get).toHaveBeenCalled();
 });
